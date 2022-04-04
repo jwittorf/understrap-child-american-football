4f7d2033a012e60394f0ea842d2cc4972b31f5b4
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-04-04 17:25:48 +0200

    [TASK] Style topnav, use default Bootstrap nav

--- global-templates/navbar-topnav.php
+++ global-templates/navbar-topnav.php
@@ -12,5 +12,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="<?php echo esc_attr( $container ); ?>">
-	<?php wp_nav_menu(['theme_location' => 'topnav']); ?>
+	<?php wp_nav_menu([
+		'theme_location' => 'topnav',
+		'menu_class' => 'nav justify-content-end',
+		'walker' => new Understrap_WP_Bootstrap_Navwalker()
+	]); ?>
 </div>
