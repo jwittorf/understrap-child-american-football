620dad8ad2eed9d5e89fd8f1f35cc3cf459412ef
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-04-11 14:12:13 +0200

    [TASK] Extend topnav, add more options and technical reference

--- global-templates/navbar-topnav.php
+++ global-templates/navbar-topnav.php
@@ -12,13 +12,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 $topnav_pos = get_theme_mod( 'understrap_child_topnav_position' );
 ?>
 
-<div class="<?php echo esc_attr( $container ); ?>">
-	<?php wp_nav_menu(
-		array (
-			'theme_location' => 'topnav',
-			'menu_class'     => 'nav ' . $topnav_pos,
-			'depth'          => 1,
-			'walker'         => new Understrap_WP_Bootstrap_Navwalker(),
-		)
-	); ?>
-</div>
+<?php if ( 'none' !== $topnav_pos ): ?>
+	<div class="<?php echo esc_attr( $container ); ?>">
+		<?php wp_nav_menu(
+			array (
+				'theme_location' => 'topnav',
+				'menu_class'     => 'nav ' . $topnav_pos,
+				'depth'          => 1,
+				'walker'         => new Understrap_WP_Bootstrap_Navwalker(),
+			)
+		); ?>
+	</div>
+<?php endif; ?>
--- includes/customizer.php
+++ includes/customizer.php
@@ -1,15 +1,16 @@
 <?php
 
-if ( ! function_exists( 'understrap_child_theme_customize_register' ) ) {
+if ( !function_exists( 'understrap_child_theme_customize_register' ) ) {
 	/**
 	 * Register individual settings through customizer's API.
 	 *
 	 * @param WP_Customize_Manager $wp_customize Customizer reference.
 	 */
-	function understrap_child_theme_customize_register( $wp_customize ) {
+	function understrap_child_theme_customize_register( $wp_customize )
+	{
 		$wp_customize->add_section(
 			'understrap_child_theme_layout_options',
-			array(
+			array (
 				'title'       => __( 'Theme Layout Settings', 'understrap_child' ),
 				'capability'  => 'edit_theme_options',
 				'description' => __( 'General theme settings', 'understrap_child' ),
@@ -19,7 +20,7 @@ if ( ! function_exists( 'understrap_child_theme_customize_register' ) ) {
 
 		$wp_customize->add_setting(
 			'understrap_child_topnav_position',
-			array(
+			array (
 				'default'           => 'justify-content-end',
 				'type'              => 'theme_mod',
 				'sanitize_callback' => 'sanitize_text_field',
@@ -31,26 +32,29 @@ if ( ! function_exists( 'understrap_child_theme_customize_register' ) ) {
 			new WP_Customize_Control(
 				$wp_customize,
 				'understrap_child_topnav_position',
-				array(
-					'label'             => __( 'Sidebar Positioning', 'understrap_child' ),
+				array (
+					'label'             => __( 'Topnav Positioning', 'understrap_child' ),
 					'description'       => __(
-						'Set topnav\'s default position. Can either be: justify-content-end, justify-content-start, both or none. Note: this can be overridden on individual pages.',
+						'Set topnav\'s default position. Reference: <a href="https://getbootstrap.com/docs/5.1/utilities/flex/#justify-content" target="_blank">Bootstrap</a> and <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/#aa-justify-content" target="_blank">Flexbox in general</a>.  Note: this can be overridden on individual pages.',
 						'understrap_child'
 					),
 					'section'           => 'understrap_child_theme_layout_options',
 					'settings'          => 'understrap_child_topnav_position',
 					'type'              => 'select',
 					'sanitize_callback' => 'understrap_child_theme_slug_sanitize_select',
-					'choices'           => array(
-						'justify-content-end' => __( 'Right topnav', 'understrap_child' ),
-						'justify-content-start'  => __( 'Left topnav', 'understrap_child' ),
-						'both'  => __( 'Left & Right topnavs', 'understrap_child' ),
-						'none'  => __( 'No topnav', 'understrap_child' ),
+					'choices'           => array (
+						'justify-content-start'   => __( 'Left topnav', 'understrap_child' ),
+						'justify-content-end'     => __( 'Right topnav', 'understrap_child' ),
+						'justify-content-center'  => __( 'Center topnav', 'understrap_child' ),
+						'justify-content-between' => __( 'Split items between topnav', 'understrap_child' ),
+						'justify-content-around'  => __( 'Items around middle topnav', 'understrap_child' ),
+						'justify-content-evenly'  => __( 'Center items evenly in topnav', 'understrap_child' ),
+						'none'                    => __( 'No topnav', 'understrap_child' ),
 					),
 					'priority'          => apply_filters( 'understrap_child_topnav_position_priority', 20 ),
 				)
 			)
 		);
-}
+	}
 } // End of if function_exists( 'understrap_child_theme_customize_register' ).
 add_action( 'customize_register', 'understrap_child_theme_customize_register' );
