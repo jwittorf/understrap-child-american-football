a61d95a56e808fe33e45be79f37bd4b273bb8f8d
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-05-16 17:05:07 +0200

    [TASK] Output content on homepage (to make use of elementor)

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -48,6 +48,18 @@ if ( is_front_page() ) {
 						}
 						?>
 
+						<?php
+						while ( have_posts() ) {
+							the_post();
+							get_template_part( 'loop-templates/content', 'page' );
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) {
+								comments_template();
+							}
+						}
+						?>
+
 					</main><!-- #main -->
 
 				</div><!-- #primary -->
