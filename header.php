994b301016dc98ca56f0cf8cc2b0dfda0525981b
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-05-23 15:42:42 +0200

    [TASK] Make header sticky to the top (fixed after scrolling)

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ $navbar_type = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
-	<header id="wrapper-navbar">
+	<header id="wrapper-navbar" class="sticky-top">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e(
 				'Skip to content',
