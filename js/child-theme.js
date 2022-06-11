b74eff5f12a00c4a06a7bb4c587318fb690c2ada
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-06-11 18:58:44 +0200

    [TASK] Run the dist task to compile js and css

--- js/child-theme.js
+++ js/child-theme.js
@@ -9296,6 +9296,11 @@
 	  }
 	})();
 
+	// Add your custom JS here.
+	jQuery(document).ready(function () {
+	  jQuery('.mstw-lm-table').addClass('table table-bordered table-striped table-hover').children('tbody').addClass('table-group-divider');
+	});
+
 	exports.Alert = alert;
 	exports.Button = button;
 	exports.Carousel = carousel;
