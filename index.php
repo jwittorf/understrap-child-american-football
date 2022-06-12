f6b2b25a18cd3c1fa8d4719a2d035b16e9150722
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-06-12 14:01:14 +0200

    [FEATURE] Adjust index.php for news/blog page

--- index.php
+++ index.php
@@ -23,48 +23,59 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<?php get_template_part( 'global-templates/hero' ); ?>
 <?php endif; ?>
 
-<div class="wrapper" id="index-wrapper">
+	<div class="wrapper" id="index-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+		<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+			<div class="row">
 
-			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+				<!-- Do the left sidebar check and opens the primary div -->
+				<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-			<main class="site-main" id="main">
+				<main class="site-main" id="main">
 
-				<?php
-				if ( have_posts() ) {
-					// Start the Loop.
-					while ( have_posts() ) {
-						the_post();
+					<div class="d-flex align-items-center news-hero">
+						<?php echo get_the_post_thumbnail($wp_query->queried_object_id); ?>
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
+						<header class="entry-header">
+							<h1 class="entry-title">
+								<?php echo get_the_title($wp_query->queried_object_id); ?>
+							</h1>
+						</header><!-- .entry-header -->
+
+					</div>
+
+					<?php
+					if ( have_posts() ) {
+						// Start the Loop.
+						while ( have_posts() ) {
+							the_post();
+
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'loop-templates/content', get_post_format() );
+						}
+					} else {
+						get_template_part( 'loop-templates/content', 'none' );
 					}
-				} else {
-					get_template_part( 'loop-templates/content', 'none' );
-				}
-				?>
+					?>
 
-			</main><!-- #main -->
+				</main><!-- #main -->
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
+				<!-- The pagination component -->
+				<?php understrap_pagination(); ?>
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+				<!-- Do the right sidebar check -->
+				<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
-		</div><!-- .row -->
+			</div><!-- .row -->
 
-	</div><!-- #content -->
+		</div><!-- #content -->
 
-</div><!-- #index-wrapper -->
+	</div><!-- #index-wrapper -->
 
 <?php
 get_footer();
