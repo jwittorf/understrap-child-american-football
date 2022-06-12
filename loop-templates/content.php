49b034dd213442941d74aaf8d7060b3efa5a47c3
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2022-06-12 20:47:22 +0200

    [TASK] Improve news and social feed (instagram) layout

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -13,11 +13,16 @@ defined( 'ABSPATH' ) || exit;
 
 	<article <?php post_class( 'mb-5' ); ?> id="post-<?php the_ID(); ?>">
 
-	<div class="position-relative">
-		<?php echo get_the_post_thumbnail( $post->ID ); ?>
-		<header class="entry-header px-3 mb-0 <?php echo (has_post_thumbnail()) ? 'position-absolute' : '' ?>"
+	<div class="row">
+		<div class="col-md-6">
+			<?php echo get_the_post_thumbnail( $post->ID ); ?>
+		</div>
+		<header class="entry-header px-3 mb-0 <?php echo (has_post_thumbnail()) ? 'col-md-6' : '' ?>"
 		        style="bottom: 0;">
-			<?php the_title( '<h2 class="h1 entry-title">','</h2>' ); ?>
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark" class="text-white text-decoration-none">',
+				esc_url( get_permalink() ) ),
+				'</a></h2>' )
+			; ?>
 
 			<?php if ( 'post' === get_post_type() ) : ?>
 
